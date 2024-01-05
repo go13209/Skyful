c4fe07ff800f3a388f8e6d5760432b7a43859aab
Rilee-0320 <go13209@naver.com>
2024-01-06 03:17:46 +0900

    [FEAT] post create template

---
+++ static/js/create.js
@@ -0,0 +1,23 @@
+document
+  .querySelectorAll('.image-container input[type="file"]')
+  .forEach(function (imageInput) {
+    imageInput.style.display = "none";
+    imageInput.addEventListener("change", function () {
+      previewImage(this);
+    });
+  });
+
+function previewImage(input) {
+  const previewImage = input.parentElement.querySelector(".postImage");
+  if (input.files.length > 0) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      previewImage.src = e.target.result;
+      previewImage.style.display = "block";
+    };
+    reader.readAsDataURL(input.files[0]);
+  } else {
+    previewImage.style.display = "none";
+    previewImage.src = "";
+  }
+}

c0b52e6afcec63c960f8a0d6aecd8ff19cd613fe
Rilee-0320 <go13209@naver.com>
2024-01-09 23:24:56 +0900

    [FIX] accounts user model

--- accounts/models.py
+++ accounts/models.py
@@ -6,13 +6,12 @@ from imagekit.processors import ResizeToFill
 
 class User(AbstractUser):
     def image_path(instance, filename):
-        return f"accounts/{instance.username}/{filename[:10]}"
+        return f"accounts/{instance.username}/{filename}"
 
     nickname = models.CharField(max_length=20)
     profile_img = ProcessedImageField(
         upload_to=image_path,
         processors=[ResizeToFill(200, 200)],
-        format="JPEG",
         options={"quality": 90},
         null=True,
         blank=True,

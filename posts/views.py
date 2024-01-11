40963099f1fb8c24094478737a0b2fbf90568e35
Rilee-0320 <go13209@naver.com>
2024-01-12 00:06:33 +0900

    [FIX] posts create

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.forms import modelformset_factory
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from .forms import PostForm, PostImageForm, CommentForm
-from .models import Post, PostImage
+from .forms import PostForm, CommentForm
+from .models import Post
 from datetime import datetime
 
 
@@ -20,36 +19,21 @@ def create(request, post_date):
     User = get_user_model()
     person = User.objects.get(pk=request.user.pk)
     post_date = datetime.strptime(post_date, "%Y-%m-%d").date()
-    ImageFormSet = modelformset_factory(
-        PostImage,
-        form=PostImageForm,
-        extra=5,  # 동적으로 필요한 개수의 폼이 나타나도록 수정
-    )
 
     if request.method == "POST":
-        postForm = PostForm(request.POST)
-        formset = ImageFormSet(
-            request.POST, request.FILES, queryset=PostImage.objects.none()
-        )
-        if postForm.is_valid() and formset.is_valid():
-            post_form = postForm.save(commit=False)
-            post_form.user = request.user
-            post_form.public = request.POST.get("public") == "on"
-            post_form.save()
-
-            for form in formset.cleaned_data:
-                if form:
-                    image = form["image"]
-                    photo = PostImage(post=post_form, image=image)
-                    photo.save()
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.public = request.POST.get("public") == "on"
+            post.save()
 
             shared_users = request.POST.getlist("shared_with")
-            post_form.shared_with.set(shared_users)
+            post.shared_with.set(shared_users)
 
             return redirect("posts:main")
     else:
         form = PostForm(initial={"date": post_date})
-        formset = ImageFormSet(queryset=PostImage.objects.none())
 
     following = person.followings.all()
     followers = person.followers.all()
@@ -57,7 +41,6 @@ def create(request, post_date):
 
     context = {
         "form": form,
-        "formset": formset,
         "date": post_date,
         "friends": friends,
     }
@@ -76,10 +59,8 @@ def check_post(request, post_date):
 @login_required
 def detail(request, user_pk, post_date):
     post = Post.objects.get(user=user_pk, date=post_date)
-    images = PostImage.objects.filter(post=post)
     context = {
         "post": post,
-        "images": images,
         "form": CommentForm(),
     }
     return render(request, "posts/detail.html", context)

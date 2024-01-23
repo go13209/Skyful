f4672c97683753a13db0a1342cdf41b413966121
Rilee-0320 <go13209@naver.com>
2024-01-23 21:33:41 +0900

    [STYLE] accounts password_change template

---
+++ static/js/password_change.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const password1Input = document.getElementById("id_new_password1");
+  const password2Input = document.getElementById("id_new_password2");
+  const mismatchMessageElement = document.getElementById("passwordMismatchMessage");
+
+  function checkPasswordMatch() {
+    const password1 = password1Input.value;
+    const password2 = password2Input.value;
+
+    if (password1 !== password2) {
+      mismatchMessageElement.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> 비밀번호가 일치하지 않습니다.`;
+      mismatchMessageElement.style.color = "red";
+    } else {
+      mismatchMessageElement.textContent = "";
+    }
+  }
+
+  password1Input.addEventListener("input", checkPasswordMatch);
+  password2Input.addEventListener("input", checkPasswordMatch);
+});

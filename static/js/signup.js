328c88da372b69380a7a8032e099ffbc59dbaeca
Rilee-0320 <go13209@naver.com>
2024-02-11 19:25:04 +0900

    [STYLE] accounts signup template

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,5 +1,6 @@
+const profileImagePreview = document.querySelector(".preview");
+
 function previewImage(input) {
-  const profileImagePreview = document.querySelector(".preview");
   if (input.files.length > 0) {
     const reader = new FileReader();
     reader.onload = function (e) {
@@ -13,105 +14,204 @@ function previewImage(input) {
   }
 }
 
-window.onload = function () {
-  const profileImagePreview = document.querySelector(".preview");
-  profileImagePreview.style.display = "none";
-  profileImagePreview.src = "";
-};
+function validatePassword(password) {
+  const passwordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  return passwordRegex.test(password);
+}
 
 document.addEventListener("DOMContentLoaded", function () {
   const usernameInput = document.getElementById("id_username");
   const nicknameInput = document.getElementById("id_nickname");
+  const password1Input = document.getElementById("id_password1");
+  const password2Input = document.getElementById("id_password2");
+  const usernameLengthCount = document.getElementById("username-length-count");
+  const nicknameLengthCount = document.getElementById("nickname-length-count");
   const usernameMessageElement = document.getElementById("username-message");
   const nicknameMessageElement = document.getElementById("nickname-message");
+  const validationMessageElement = document.getElementById(
+    "passwordValidationMessage"
+  );
+  const mismatchMessageElement = document.getElementById(
+    "passwordMismatchMessage"
+  );
+  const signUpButton = document.getElementById("signup-button");
 
-  function checkDuplicate(input, type, messageElement) {
+  profileImagePreview.style.display = "none";
+  profileImagePreview.src = "";
+
+  // 중복 검사
+  async function checkDuplicate(input, type, messageElement) {
     const value = input.value.trim();
 
     if (value === "") {
       messageElement.textContent = "";
-      return;
+      return false;
     }
 
     const formData = new FormData();
     formData.append(type, value);
 
-    fetch("/accounts/check_duplicate/", {
+    const response = await fetch("/accounts/check_duplicate/", {
       method: "POST",
       body: formData,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data[type].exists) {
-          messageElement.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> ${data[type].message}`;
-          messageElement.style.color = "red";
-        } else {
-          messageElement.textContent = "";
-        }
-      });
+    });
+
+    const data = await response.json();
+
+    if (data[type].exists) {
+      messageElement.innerHTML = `(${data[type].message})`;
+      messageElement.style.color = "red";
+      return false;
+    } else {
+      messageElement.textContent = "";
+      return true;
+    }
   }
 
-  usernameInput.addEventListener("blur", function () {
-    checkDuplicate(usernameInput, "username", usernameMessageElement);
-  });
+  // 모든 유효성 검사를 실행
+  async function validateAll() {
+    const isUsernameValid =
+      (await checkDuplicate(
+        usernameInput,
+        "username",
+        usernameMessageElement
+      )) && checkLength(usernameInput, usernameLengthCount);
+    const isNicknameValid =
+      (await checkDuplicate(
+        nicknameInput,
+        "nickname",
+        nicknameMessageElement
+      )) && checkLength(nicknameInput, nicknameLengthCount);
+    const isPasswordValid = await validatePasswordInput();
+    const isPasswordMatch = checkPasswordMatch();
 
-  nicknameInput.addEventListener("blur", function () {
-    checkDuplicate(nicknameInput, "nickname", nicknameMessageElement);
-  });
-});
+    // 모든 조건을 만족하면 버튼을 활성화
+    if (
+      isUsernameValid &&
+      isNicknameValid &&
+      isPasswordValid &&
+      isPasswordMatch
+    ) {
+      signUpButton.removeAttribute("disabled");
+    } else {
+      signUpButton.setAttribute("disabled", true);
+    }
+  }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const password1Input = document.getElementById("id_password1");
-  const password2Input = document.getElementById("id_password2");
-  const mismatchMessageElement = document.getElementById(
-    "passwordMismatchMessage"
-  );
+  // 아이디, 닉네임 길이 검사
+  function checkLength(input, lengthCount) {
+    const length = input.value.length;
 
+    if (length <= 20) {
+      lengthCount.textContent = `(${length}/20)`;
+      lengthCount.style.color = "grey";
+      return true;
+    } else {
+      lengthCount.textContent = `(${length}/20)`;
+      lengthCount.style.color = "red";
+      return false;
+    }
+  }
+
+  // 비밀번호 유효성 검사
+  async function validatePasswordInput() {
+    const password = password1Input.value;
+
+    if (!validatePassword(password)) {
+      // 비밀번호가 유효하지 않을 경우 사용자에게 알려줌
+      validationMessageElement.textContent =
+        "(비밀번호는 최소 8자 이상의 길이로, 알파벳, 숫자, 특수문자(@$!%*#?&)를 포함해야 합니다.)";
+      validationMessageElement.style.color = "red";
+      return false;
+    } else {
+      validationMessageElement.textContent = "";
+      return true;
+    }
+  }
+
+  // 비밀번호, 비밀번호 입력 일치 검사
   function checkPasswordMatch() {
     const password1 = password1Input.value;
     const password2 = password2Input.value;
 
     if (password1 !== password2) {
-      mismatchMessageElement.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> 두 비밀번호가 일치하지 않습니다.`;
+      mismatchMessageElement.innerHTML = "(비밀번호가 일치하지 않습니다.)";
       mismatchMessageElement.style.color = "red";
+      return false;
     } else {
       mismatchMessageElement.textContent = "";
+      return true;
     }
   }
 
-  password1Input.addEventListener("input", checkPasswordMatch);
-  password2Input.addEventListener("input", checkPasswordMatch);
-});
+  function allInputsFilled() {
+    return (
+      usernameInput.value.trim() !== "" &&
+      nicknameInput.value.trim() !== "" &&
+      password1Input.value.trim() !== "" &&
+      password2Input.value.trim() !== ""
+    );
+  }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const usernameInput = document.getElementById("id_username");
-  const usernameLengthCount = document.getElementById("username-length-count");
-  const nicknameInput = document.getElementById("id_nickname");
-  const nicknameLengthCount = document.getElementById("nickname-length-count");
-
-  function usernameCheckLength() {
-    const usernameLength = usernameInput.value.length;
-
-    if (usernameLength <= 20) {
-      usernameLengthCount.textContent = `(${usernameLength}/20)`;
-      usernameLengthCount.style.color = "grey";
+  // 이벤트 리스너
+  usernameInput.addEventListener("blur", () => {
+    checkDuplicate(usernameInput, "username", usernameMessageElement);
+    checkLength(usernameInput, usernameLengthCount);
+    if (allInputsFilled()) {
+      validateAll();
     } else {
-      usernameLengthCount.textContent = `(${usernameLength}/20)`;
-      usernameLengthCount.style.color = "red";
+      signUpButton.setAttribute("disabled", true);
     }
-  }
+  });
 
-  function nicknameCheckLength() {
-    const nicknameLength = nicknameInput.value.length;
-
-    if (nicknameLength <= 20) {
-      nicknameLengthCount.textContent = `(${nicknameLength}/20)`;
-      nicknameLengthCount.style.color = "grey";
+  nicknameInput.addEventListener("blur", () => {
+    checkDuplicate(nicknameInput, "nickname", nicknameMessageElement);
+    checkLength(nicknameInput, nicknameLengthCount);
+    if (allInputsFilled()) {
+      validateAll();
     } else {
-      nicknameLengthCount.textContent = `(${nicknameLength}/20)`;
-      nicknameLengthCount.style.color = "red";
+      signUpButton.setAttribute("disabled", true);
     }
-  }
-  usernameInput.addEventListener("input", usernameCheckLength);
-  nicknameInput.addEventListener("input", nicknameCheckLength);
+  });
+
+  usernameInput.addEventListener("input", () => {
+    checkLength(usernameInput, usernameLengthCount);
+    if (allInputsFilled()) {
+      validateAll();
+    } else {
+      signUpButton.setAttribute("disabled", true);
+    }
+  });
+
+  nicknameInput.addEventListener("input", () => {
+    checkLength(nicknameInput, nicknameLengthCount);
+    if (allInputsFilled()) {
+      validateAll();
+    } else {
+      signUpButton.setAttribute("disabled", true);
+    }
+  });
+
+  password1Input.addEventListener("input", () => {
+    validatePasswordInput();
+    if (allInputsFilled()) {
+      validateAll();
+    } else {
+      signUpButton.setAttribute("disabled", true);
+    }
+  });
+
+  password2Input.addEventListener("input", () => {
+    checkPasswordMatch();
+    if (allInputsFilled()) {
+      validateAll();
+    } else {
+      signUpButton.setAttribute("disabled", true);
+    }
+  });
+
+  password2Input.addEventListener("blur", () => {
+    validateAll();
+  });
 });
